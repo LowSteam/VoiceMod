3600ea542854b64f855730befe078aed5dac8c64
Talmage Wright <lowsteam@Talmages-MacBook-Air.local>
2025-02-21 21:29:07 -0600

    Initial commit

---
+++ src/main/java/net/lowsteam/voicemod/VoiceMod.java
@@ -0,0 +1,17 @@
+package net.lowsteam.voicemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VoiceMod implements ModInitializer {
+
+	public static final String MOD_ID = "voicemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/lowsteam/voicemod/VoiceModClient.java
@@ -0,0 +1,10 @@
+package net.lowsteam.voicemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoiceModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // TODO: Implement client initialization logic here
+    }
+}
